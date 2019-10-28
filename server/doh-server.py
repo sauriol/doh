2bf2f00fac6158ff9699820cf8d672b9e100a46e
Jack Sauriol <sauriol@csh.rit.edu>
2019-10-28 12:40:40 -0400

    Base of get request

--- server/doh-server.py
+++ server/doh-server.py
@@ -3,11 +3,15 @@
 from quart import Quart, abort, request
 
 app = Quart(__name__)
+allowed_content_types = ['application/dns-message']
 
 @app.route('/dns', methods=['GET', 'POST'])
 async def dns():
     if request.method == 'GET':
-        return 'GET'
+        if request.headers.get('Content-Type') not in allowed_content_types:
+            abort(415)
+        data = request.get_data()
+        req = bytes(data)
     elif request.method == 'POST':
         return 'POST'
     else:
