2df3fdf5351c5738a515061044a2c39e218e19c5
Jack Sauriol <sauriol@csh.rit.edu>
2019-10-28 15:35:04 -0400

    Add DNS message parsing for POST

--- server/doh-server.py
+++ server/doh-server.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
-from quart import Quart, abort, request
+from quart import Quart, abort, request, Response
+import dns.query
 
 app = Quart(__name__)
 allowed_content_types = ['application/dns-message']
 
 @app.route('/dns', methods=['GET', 'POST'])
-async def dns():
-    if request.method == 'GET':
+async def serve():
+    if request.method == 'POST':
         if request.headers.get('Content-Type') not in allowed_content_types:
             abort(415)
-        data = request.get_data()
+        data = await request.get_data()
         req = bytes(data)
-    elif request.method == 'POST':
-        return 'POST'
+    elif request.method == 'GET':
+        return 'GET'
     else:
         abort(405)
+
+    try:
+        message = dns.message.from_wire(req)
+    except Exception as e:
+        print(e)
+        return Response('Error parsing DNS message: ' + str(e),
+                status=400, mimetype='text/plain')
 
 
 if __name__ == '__main__':
